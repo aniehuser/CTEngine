72b1d3129bbddbda38b7e904f390f5926c4d5747
Anthony Niehuser <tniehuser@gmail.com>
2018-01-17 14:39:50 -0800

    created library and test projects

---
+++ ct-engine/ct-engine-test/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ct_engine;
+
+namespace ct_engine_test
+{
+	public class Test
+    {
+        public static void Main(string[] args)
+        {
+            Class1 test = new Class1();        
+        }
+    }
+}
---
+++ ct-engine/ct-engine/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ct_engine
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("HEY thEre");
+        }
+    }
+
+}
